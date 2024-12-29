c0a93a6296bf15a6018616c103185a2fe10edc80
Delvingt <delvingt.julien@gmail.com>
2024-12-29 22:58:24 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -29,5 +29,58 @@
         return false;
     });
 
+    // Header carousel
+    $(document).ready(function() {
+
+        $("#header_carousel").owlCarousel({
+            autoplay:true,
+            autoPlay: 2000, //Set AutoPlay to 3 seconds
+            dots: true,
+            loop: true,
+
+            items : 1,
+            itemsDesktop : [1199,3],
+            itemsDesktopSmall : [979,3]
+
+        });
+
+    });
+
+
+    // Gallery Index carousel
+    $(document).ready(function() {
+
+        $("#gallery_index_carousel").owlCarousel({
+            autoPlay: 3000, //Set AutoPlay to 3 seconds
+            items : 4,
+            itemsDesktop : [1199,3],
+            itemsDesktopSmall : [979,3]
+
+        });
+
+    });
+
+    // Reviews Index carousel
+    $(".reviews_carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1000,
+        center: true,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+
 })(jQuery);
 
